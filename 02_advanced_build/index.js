b773edef431bb14d6fee885596d7f5d50241fbbe
BBHT Beratungsgesellschaft mbH & Co. KG <109295690+bbhtde@users.noreply.github.com>
2025-05-27 16:43:54 +0200

    Update index.js
    Added SIGTERM / SIGINT-Handler

--- 02_advanced_build/index.js
+++ 02_advanced_build/index.js
@@ -3,6 +3,9 @@ const os = require('os')
 const app = express()
 let requestCount = 0;
 
+// Is delivered on Windows when Ctrl+Break is pressed. 
+// On non-Windows platforms, it can be listened on, but there 
+// is no way to send or generate it.
 app.get('/api/count', (req, res) => {
   res.send(createAndGetResponse(requestCount));
   addRequestCount();
@@ -12,7 +15,7 @@ app.get('/api/health', (req, res) => {
   res.send({'healthy': 'true'})
 })
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.info("startup complete")
 });
 
@@ -29,3 +32,22 @@ function createAndGetResponse(countValue) {
 function addRequestCount() {
   requestCount = requestCount + 1;
 }
+
+// Hier kommt die Ergänzung für die Reaktion auf Strg+C
+process.on('SIGINT', () => {
+  console.info('\nSIGINT signal received. Shutting down gracefully...');
+  server.close(() => {
+    console.info('HTTP server closed.');
+    // Hier könntest du weitere Aufräumarbeiten durchführen, z.B. Datenbankverbindungen schließen
+    process.exit(0); // Beende den Prozess
+  });
+});
+
+// Optional: Für SIGTERM (z.B. bei Docker-Stopp)
+process.on('SIGTERM', () => {
+  console.info('\nSIGTERM signal received. Shutting down gracefully...');
+  server.close(() => {
+    console.info('HTTP server closed.');
+    process.exit(0);
+  });
+});
